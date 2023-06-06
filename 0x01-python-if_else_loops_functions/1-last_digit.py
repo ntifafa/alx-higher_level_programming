35c4f74180e9ca0f8b8a47c6955fcd62b94e984b
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-06 13:57:28 +0000

    added file for task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+# convert int to str
+output = str(number)
+# slice last digit and assign
+last_digit = int(output[-1])
+
+if last_digit > 5:
+    print(f'last digit of {number} is {last_digit} and is greater than 5')
+
+elif last_digit == 0:
+    print(f'last digit of {number} is {last_digit} and is 0')
+
+elif last_digit < 6 and last_digit != 0:
+    print(f'last digit of {number} is {last_digit} and is less than 6 \
+and not 0')
