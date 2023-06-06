fa6097caff0dcb1b16eab2549e1c23ed9b15ae86
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-06 15:00:52 +0000

    updated file for task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,22 +2,22 @@
 import random
 number = random.randint(-10000, 10000)
 
-# convert int to str
-output = str(number)
-# slice last digit and assign
-last_digit = int(output[-1])
+# # convert int to str
+# output = str(number)
+# # slice last digit and assign
+# last_digit = int(output[-1])
+
 # ensure negative output carries through to last digit
 if number < 0:
-    last_digit = -last_digit
-    print(f'last digit of {number} is {last_digit} and is less than 6 \
-and not 0')
+    print(f'last digit of {number} is -{(number * -1) % 10} \
+and is less than 6 and not 0')
 
-elif last_digit > 5:
-    print(f'last digit of {number} is {last_digit} and is greater than 5')
+elif number % 10 > 5:
+    print(f'last digit of {number} is {number % 10} and is greater than 5')
 
-elif last_digit == 0:
-    print(f'last digit of {number} is {last_digit} and is 0')
+elif number % 10 == 0:
+    print(f'last digit of {number} is {number % 10} and is 0')
 
-elif last_digit < 6 and last_digit != 0:
-    print(f'last digit of {number} is {last_digit} and is less than 6 \
+elif number % 10 < 6 and number % 10 != 0:
+    print(f'last digit of {number} is {number % 10} and is less than 6 \
 and not 0')
