755a06d71ebe6e8b784d9c10a50bc3a9a1fe6ae3
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-06 07:39:45 +0000

    added file for task 100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-def main():
-    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-    sys.exit(1)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
