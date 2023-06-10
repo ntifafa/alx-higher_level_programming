88f06b524160ace863af1026ca35e092e4d502c3
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-10 13:47:25 +0000

    added file or task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+
+    if idx > len(my_list):
+        return None
+
+    else:
+        for i in my_list:
+            if i == idx:
+                return my_list[i]
