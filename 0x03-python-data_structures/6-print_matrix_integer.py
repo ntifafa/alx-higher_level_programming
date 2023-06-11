a9e3ea162dc18803985ed2a8e8a23c44ddbf5f41
Nutifafa Foli-Biaku <122826615+ntifafa@users.noreply.github.com>
2023-06-11 11:14:48 +0000

    Updated 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            if element == row[-1]:
-                print("{:d}".format(element), end=" ")
-            else:
-                print("{:d}".format(element), end=" ")
+             print("{:d}".format(element), end=" ")
         print()
