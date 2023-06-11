ea48859513fec52a8008e0bbff74879ae5d8693e
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-11 10:40:56 +0000

    updated file for task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
             print("{:d}".format(element), end=" ")
+        if element != row[-1]:
+                print("{:d}".format(element), end="")
         print()
