fe2cc2f9132e7b0e23602614ced758ef98ebb0dd
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-14 17:06:24 +0000

    updated file for task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    output = 0
+    prev_val = 0
+
+    for digit in roman_string[::-1]:
+        val = roman_dict.get(digit, 0)
+        if val >= prev_val:
+             output += val
+        else:
+            output -= val
+        prev_val = val
+    return output
