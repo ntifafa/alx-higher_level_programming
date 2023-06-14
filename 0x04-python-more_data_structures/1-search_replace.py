e8a62e2037695e7e7c81c97aff248239d5e72fe8
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-14 13:27:49 +0000

    updated file for task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+    new_list = my_list[:]
     for i, val in enumerate(my_list):
         if val == search:
-            my_list[i] = replace
-    return my_list
+            new_list[i] = replace
+    return new_list
