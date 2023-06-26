9378fcefa830de0900ddb597e630f958f99163e9
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-26 17:20:14 +0000

    added file for task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
+
+    new_list = []  # create new list
+    quo = None  # declare variable for output
+
     for i in range(list_length):
+        if quo == TypeError or ZeroDivisionError or IndexError:
+            quo = 0  # assign 0 to quo when above errors are encountered
         try:
             quo = my_list_1[i] / my_list_2[i]
         except TypeError:
