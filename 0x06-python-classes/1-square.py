02d3c34f5812c904ed2201b79e09066235cce007
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-27 16:00:43 +0000

    added file for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize an instance of a square
+        Args: size - is the size of the square
+        """
+
+        self.__size = size
