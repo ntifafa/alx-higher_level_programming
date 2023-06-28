0deb4ad672621903950dd1601d222b447cbad727
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-28 22:38:56 +0000

    added file for task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize an instance of a square
+
+        Args:
+            size - is the size of the square
+            and defaults to 0 if nothing is passed.
+            Must be an integer not less than 0.
+
+        Raises:
+            TypeError - when size passed is not an integer.
+            ValueError - when size is less than 0."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Computes and returns the area of the square
+
+        Returns:
+            Area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Retrieves private instance attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets private instance attribute.
+        Raises:
+            TypeError - when size passed is not an integer.
+            ValueError - when size is less than 0."""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
