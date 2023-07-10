5e44081b940b52338031f69400cd529aa976d207
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-07-10 23:00:58 +0000

    updated file for task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,13 +4,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    "defines a square class"
+    """defines a square class"""
     def __init__(self, size):
-        "constructor for sqaure instance"
+        """constructor for sqaure instance"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-    
+
     def area(self):
-        "returns area of the square"
+        """returns area of the square"""
         return self.__size ** 2
