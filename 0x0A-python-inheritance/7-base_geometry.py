b31c4679ad83a3e94a1ade8c914333b343723261
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-07-10 18:41:43 +0000

    added files for task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate that value is an int and not less than 0"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
