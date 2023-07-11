987ba2517d618a3f5df219bfbaafbcf057304f98
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-07-11 18:47:18 +0000

    added file for task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+this script adds arguments to a python
+list and then saves to a JSON file
+"""
+import sys
+
+save_to_json = __import__('5-save_to_json_file').save_to_json_file
+load_from_json =\
+    __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item(args, filename):
+    """
+        add_item - adds all items and saves to a JSON file
+        @args: arguments that to be added.
+        @filename: file to be updated
+    """
+    try:
+        items = load_from_json(filename)
+
+    except FileNotFoundError:
+        items = []
+
+    for item in args:
+        items.append(item)
+    save_to_json(items, filename)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    filename = "add_item.json"
+    add_item(args, filename)
