60899a4fa726685119fed64b97294a97c5cca2f0
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-07-17 23:00:42 +0000

    updated parent class with dict to json converter

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 module is a base class for all other classes
 """
+import json
 
 
 class Base:
@@ -15,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """converts a dictionary into a json string"""
+        if list_dictionaries is None:
+            return '[]'
+        return json.dumps(list_dictionaries)
