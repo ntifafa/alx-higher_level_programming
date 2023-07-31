637bb93de605c8d3dc9cee9df5f0e374ccfdf404
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-07-31 15:59:40 +0000

    updated test file for task 5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
-"""module to find the max integer in a list"""
+"""The print square module."""
+import unittest
+from your_module import max_integer
 
 
-def max_integer(list=[]):
-    """
-        function to find and return the max integer in 
-        a list of integers, if the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+class MaxIntegerTestCase(unittest.TestCase):
+    def test_empty_list(self):
+        result = max_integer([])
+        self.assertIsNone(result, "Empty list should return None")
+
+    def test_single_element_list(self):
+        result = max_integer([5])
+        self.assertEqual(result, 5, "Single element list" +
+                        " should return the element itself")
+
+    def test_positive_numbers(self):
+        result = max_integer([1, 2, 3, 4, 5])
+        self.assertEqual(result, 5, "List of" +
+                         " positive numbers should return the maximum")
+
+    def test_negative_numbers(self):
+        result = max_integer([-1, -2, -3, -4, -5])
+        self.assertEqual(result, -1, "List of negative"
+                         + " numbers should return the maximum")
+
+    def test_mixed_numbers(self):
+        result = max_integer([-5, 0, 10, -2, 7])
+        self.assertEqual(result, 10, "List of mixed positive"
+                         + " and negative numbers should return the maximum")
+
+    def test_duplicate_numbers(self):
+        result = max_integer([3, 2, 5, 2, 5])
+        self.assertEqual(result, 5, "List with"
+                         + " duplicate numbers should return the maximum")
+
+if __name__ == '__main__':
+    unittest.main()
