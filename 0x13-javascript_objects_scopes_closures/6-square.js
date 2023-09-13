6f7066ddd165899caeba75387cd11608e85e7ba9
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-09-13 05:04:04 +0000

    updated file for task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
-const square = require('./5-square');
+const PrevSquare = require('./5-square');
 
-class Square extends square {
+class Square extends PrevSquare {
   charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
+    const myChar = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += myChar;
+        y++;
       }
+
+      console.log(myVar);
     }
   }
 }
+
 module.exports = Square;
