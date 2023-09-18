4f590d57ec3072bb8c9add305e987cfea502eb66
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-09-18 05:50:29 +0000

    updated file for task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,32 +5,28 @@ argument and lists all cities of that
 state, using the database `hbtn_0e_4_usa`.
 """
 
-import MySQLdb as db
-from sys import argv
-
 if __name__ == "__main__":
     """
     Access database and get
     the cities from the database.
     """
 
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_connect = db.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+        )
 
     with db_connect.cursor() as db_cursor:
         db_cursor.execute("""
-            SELECT
-                cities.id, cities.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
-            ORDER BY
-                cities.id ASC
+            SELECT cities.id, cities.name
+            FROM cities
+            JOIN states
+            ON cities.state_id = states.id
+            WHERE states.name LIKE BINARY %(state_name)s
+            ORDER BY cities.id ASC
         """, {
             'state_name': argv[4]
         })
