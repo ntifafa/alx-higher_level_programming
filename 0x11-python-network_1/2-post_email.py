dcda84fba7fb6c3519938052aaa3c25ac53c278f
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-09-29 23:54:47 +0000

    updated file for task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,18 +4,20 @@ takes a URL and an email, sends a POST request to the passed URL
 with the email as a parameter, and displays the body of
 the response (decoded in utf-8)
 """
+import sys
 import urllib.parse
 import urllib.request
-import sys
+
 
 
 if __name__ == "__main__":
-    # Get the url from the command line
+    # get the url from the command line
     url = sys.argv[1]
-    # Get the email from the command line
+    # get the email from the command line
     email = sys.argv[2]
-    # Encode the email
+    # encode the email
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    # Send the request
+    # send the request
     with urllib.request.urlopen(url, data) as response:
-        print(response.read().decode('utf-8'))
+        content = response.read().decode('utf-8')
+        print("{}".format(content))
