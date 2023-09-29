0c1476466dce7c4ed6ba6025bd1596119969be9d
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-09-29 23:40:04 +0000

    added file for task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+takes a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8), while
+managing the error.
+"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    # grab url from the command line
+    url = sys.argv[1]
+    try:
+        # send the request and get the response
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    # handle the error
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
