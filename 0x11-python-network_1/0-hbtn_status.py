8c82e6273fa4dddb4a6cb57da5b84b146a96cdb0
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-09-29 23:24:05 +0000

    added file for task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == "__main__":
+    # Get the url from the command line
+    url = 'https://alx-intranet.hbtn.io/status'
+    # Send a request to the URL using urllib
+    with urllib.request.urlopen(url) as response:
+        # Get the body of the response
+        html = response.read()
+        # Print the body of the response
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
