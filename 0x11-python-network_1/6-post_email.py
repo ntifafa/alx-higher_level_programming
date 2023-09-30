b6915949b1dcf217144d3d2b528049ae3e32a6b8
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-09-30 00:06:16 +0000

    added file for task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""takes a URL and an email address, sends a POST request to the
+ passed URL with the email as a parameter, and displays the body of the response
+ ."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    # grab the url from the command line
+    url = sys.argv[1]
+    # grab the email from the command line
+    email = sys.argv[2]
+    # store the email in a dictionary
+    data = {'email': email}
+    # send the request and get the response
+    response = requests.post(url, data=data)
+    # print the response body
+    print("{}".format(response.text))
