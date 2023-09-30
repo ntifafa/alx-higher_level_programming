1bbc1c9ea6f94c7b7575f241c30617105f829ae9
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-09-30 00:08:34 +0000

    updated file for task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""takes a URL and an email address, sends a POST request to the
- passed URL with the email as a parameter, and displays the body of the response
- ."""
+"""
+takes a URL and an email address, sends a
+POST request to the passed URL with the email
+as a parameter, and displays the body of the response
+"""
 import sys
 import requests
 
