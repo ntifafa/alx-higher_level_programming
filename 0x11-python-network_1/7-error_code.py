57393b5de9a3d581345e0c8d08b01eba2a767a09
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-09-30 00:18:34 +0000

    updated file for task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,14 +2,13 @@
 """
 sends a request to a given URL and displays the response body.
 """
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
     # grab URL from the command line arguments
     url = sys.argv[1]
-
     # send an HTTP GET request to the specified URL
     r = requests.get(url)
     # check if the response status code is greater than or equal to 400
